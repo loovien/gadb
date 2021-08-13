a20fb449521f491e23adaf2a2a0b2f8aff93106c
unknown <bigpao.luo@gmail.com>
2021-08-14 00:53:02 +0800

    xx

--- examples/main.go
+++ examples/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/electricbubble/gadb"
+	"github.com/loovien/gadb"
 	"log"
 	"os"
 	"strings"
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/electricbubble/gadb
+module github.com/loovien/gadb
 
 go 1.15
